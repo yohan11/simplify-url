68daa63d8ee28319f77f15ed4c553f519ef5ded1
yohan11 <yipco77@naver.com>
2024-04-03 15:46:06 +0900

    feat: 짧은 주소 목록 보여주기 기능 구현

--- pages/short-links/index.js
+++ pages/short-links/index.js
@@ -3,9 +3,20 @@ import ShortLinkList from '@/components/ShortLinkList';
 import Button from '@/components/Button';
 import Link from '@/components/Link';
 import styles from '@/styles/ShortLinkListPage.module.css';
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
 
-export default function ShortLinkListPage() {
-  const shortLinks = [];
+export async function getServerSideProps(){
+  await dbConnect();
+  const shortLinks = await ShortLink.find();
+
+  return{
+    props:{
+      shortLinks: JSON.parse(JSON.stringify(shortLinks)), // 기본적으로 mongoose에서 내려주는건 QUery 형이기 때문에 JSON 형태로 변환해주어야 한다.
+    }
+  }
+}
+export default function ShortLinkListPage({shortLinks}) {
 
   return (
     <>
