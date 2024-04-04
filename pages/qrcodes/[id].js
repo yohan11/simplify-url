169ddc2a80f31b5fbe9fd5bf59c68e58f9825f73
yohan11 <yipco77@naver.com>
2024-04-04 15:15:46 +0900

    feat: QR코드 수정, 삭제 기능 구현

--- pages/qrcodes/[id].js
+++ pages/qrcodes/[id].js
@@ -1,8 +1,37 @@
-import Head from 'next/head';
-import QRCodeForm, { QRCodeFormType } from '@/components/QRCodeForm';
-import styles from '@/styles/QRCodeEditPage.module.css';
+import Head from "next/head";
+import QRCodeForm, { QRCodeFormType } from "@/components/QRCodeForm";
+import styles from "@/styles/QRCodeEditPage.module.css";
+import axios from "@/lib/axios";
+import { useRouter } from "next/router";
+import dbConnect from "@/db/dbConnect";
+import QRCode from "@/db/models/QRCode";
 
-export default function QRCodeEditPage() {
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+
+  await dbConnect();
+  const qrcodeItem = await QRCode.findById(id);
+
+  if (qrcodeItem)
+    return {
+      props: {
+        qrcodeItem: JSON.parse(JSON.stringify(qrcodeItem)),
+      },
+    };
+  return {
+    notFound: true,
+  };
+}
+
+export default function QRCodeEditPage({ qrcodeItem }) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  async function handleSubmit(values) {
+    await axios.patch(`/qrcodes/${id}`, values);
+    router.push("/qrcodes");
+  }
+
   return (
     <>
       <Head>
@@ -10,7 +39,11 @@ export default function QRCodeEditPage() {
       </Head>
       <div className={styles.page}>
         <h1 className={styles.title}>QRCode 수정하기</h1>
-        <QRCodeForm type={QRCodeFormType.Edit} />
+        <QRCodeForm
+          type={QRCodeFormType.Edit}
+          onSubmit={handleSubmit}
+          initialValues={qrcodeItem}
+        />
       </div>
     </>
   );
--- pages/qrcodes/index.js
+++ pages/qrcodes/index.js
@@ -5,6 +5,8 @@ import Link from "@/components/Link";
 import styles from "@/styles/QRCodeListPage.module.css";
 import dbConnect from "@/db/dbConnect";
 import QRCode from "@/db/models/QRCode";
+import axios from "@/lib/axios";
+import { useState } from "react";
 
 export async function getServerSideProps() {
   await dbConnect();
@@ -17,7 +19,14 @@ export async function getServerSideProps() {
   };
 }
 
-export default function QRCodeListPage({ qrCodes }) {
+export default function QRCodeListPage({ qrCodes: initialQrCodes }) {
+  const [qrCodes, setQrCodes] = useState(initialQrCodes);
+
+  async function handleDelete(id) {
+    axios.delete(`qrcodes/${id}`);
+    setQrCodes((prev) => prev.filter((item) => item._id !== id));
+  }
+
   return (
     <>
       <Head>
@@ -30,7 +39,7 @@ export default function QRCodeListPage({ qrCodes }) {
             새로 만들기
           </Button>
         </header>
-        <QRCodeList items={qrCodes} />
+        <QRCodeList items={qrCodes} onDelete={handleDelete} />
       </div>
     </>
   );
